48e1bdce9f53f986f30e058e26df9bd31a5e6cc7
Jens Giehl <mobil.jgiehl@gmail.com>
2022-05-16 23:26:03 +0200

    Add GitHub Action, Update dependencies

--- src/main/java/de/agiehl/bga/BgaStatisticToBggCommentConverter/models/ConverterJob.java
+++ src/main/java/de/agiehl/bga/BgaStatisticToBggCommentConverter/models/ConverterJob.java
@@ -1,9 +1,9 @@
 package de.agiehl.bga.BgaStatisticToBggCommentConverter.models;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
--- src/main/java/de/agiehl/bga/BgaStatisticToBggCommentConverter/service/ConverterService.java
+++ src/main/java/de/agiehl/bga/BgaStatisticToBggCommentConverter/service/ConverterService.java
@@ -1,24 +1,23 @@
 package de.agiehl.bga.BgaStatisticToBggCommentConverter.service;
 
-import static de.agiehl.bga.BgaStatisticToBggCommentConverter.service.IsEmpty.isNotEmpty;
-import static java.util.stream.Collectors.toList;
-
-import java.util.List;
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Service;
-
 import de.agiehl.bga.BgaStatisticToBggCommentConverter.models.ConverterJob;
 import de.agiehl.bga.BgaStatisticToBggCommentConverter.models.Player;
 import de.agiehl.bga.BgaStatisticToBggCommentConverter.models.PlayerStatisticValue;
 import de.agiehl.bga.BgaStatisticToBggCommentConverter.service.converter.ConvertStyleTypes;
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+
+import static de.agiehl.bga.BgaStatisticToBggCommentConverter.service.IsEmpty.isNotEmpty;
+import static java.util.stream.Collectors.toList;
 
 @Service
+@AllArgsConstructor
 public class ConverterService {
 
-	@Autowired
 	private MessageSource msg;
 
 	public String convert(ConverterJob job, ConvertStyleTypes style, List<String> choosenStats,
@@ -29,11 +28,7 @@ public class ConverterService {
 
 		for (Player player : players) {
 			for (PlayerStatisticValue value : player.getValues()) {
-				if (choosenStats.contains(value.getKey()) && isNotEmpty(value.getValue())) {
-					value.setActive(true);
-				} else {
-					value.setActive(false);
-				}
+                value.setActive(choosenStats.contains(value.getKey()) && isNotEmpty(value.getValue()));
 			}
 		}
 
